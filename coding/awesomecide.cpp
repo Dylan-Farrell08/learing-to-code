2da6841bc519fae7eb1b55a02c2243e5679e9fd9
Dylan-Farrell08 <dylanfarrell609@gmail.com>
2026-05-12 15:21:10 +0100

    Initialize player position and improve display function
    Refactor game logic to initialize player position and update display logic.

--- coding/awesomecide.cpp
+++ coding/awesomecide.cpp
@@ -1,34 +1,46 @@
 #include <iostream>
+using namespace std;
 
 string map[10][10];
 
 
-int x_pos;
-int y_pos;
+int x_pos = 5;
+int y_pos = 5;
 bool playing = true;
-int main(){
-    display();
-    while(playing){
-        play_input();
-        display();
+
+void write_to_screen(int x,int y, string replace){
+    map[x][y] = replace;
+}
+
+void fill_display(){
+       for (int x = 0; x < sizeof(map) / sizeof(map[0]); x++){
+        for (int y = 0; y < sizeof(map[x]) / sizeof(map[x][0]); y++){
+            if (x == x_pos and y == y_pos){
+                map[x][y] = "a";
+            }
+            else{
+                map[x][y] = "0";
+            };
+        };
+    };
+}
+
+
+
+void display(){
+    for (int x = 0; x < sizeof(map) / sizeof(map[0]); x++){
+        std::cout << "\n";
+        for (int y = 0; y < sizeof(map[x]) / sizeof(map[x][0]); y++){
+            std::cout << map[x][y];
+        };
     };
 }
 
 void play_input(){
     string input;
     std::cin >> input;
-    if (input 
-}
-
-void write_to_screen(int x,int y, string replace){
-    map[x][y] = replace;
-}
-
-void display(){
-    for (int x = 0; x == sizeof(map); x++){
-        std::cout << "\n";
-        for (int y = 0; y == sizeof(map); y++){
-            std::cout << map[x][y];
-        };
+    if (input == "a"){
+        x_pos += 1;
     };
+    write_to_screen(x_pos, y_pos, "a");
 }
